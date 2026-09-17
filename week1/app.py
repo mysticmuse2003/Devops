6f03278212ca8d9d53bf7353bdda3128cca77a87
mysticmuse2003 <mysticmuse2003@gmail.com>
2026-09-17 22:43:48 +0530

    added week4

---
+++ week1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/success', methods=['POST'])
+def success():
+    username = request.form['username']
+    email = request.form['email']
+    print(username,email)
+    return render_template('success.html', username=username, email=email)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
